27dc54b38c9edea19d8a64f144f4060c5a512501
Brandon Cope <hunterv96@gmail.com>
2022-03-11 13:53:37 -0600

    sorted astronauts by hours worked. i haven't changed the json in the for loop to create astroString yet, so I'm worried about breaking the code when i do so

--- script.js
+++ script.js
@@ -4,10 +4,30 @@ addEventListener("load", function(){
     this.fetch(url).then(function(response){
         const jsonPromise = response.json();
         jsonPromise.then(function(json){
-            console.log({json});
+            //Creates an array that has to be less in length
+            //than json, which is 7. 
+            let astroArray = [];
+            while(astroArray.length<json.length){
+                let astroMostHours = {
+                    hoursInSpace: -1
+                };  
+                for(let i=0;i<json.length;i++){
+                    if(!astroArray.includes(json[i])&&json[i].hoursInSpace>astroMostHours.hoursInSpace){
+                        astroMostHours=json[i];
+                    }
+                }
+                astroArray.push(astroMostHours);
+            }
+
+
             let container = document.getElementById("container");
             let astroString = "";
-            for(let i=0; i<json.length; i++){
+            let i = 0;
+            //Creates the skill string to fill inside of the 
+            //HTML and adds it to astroString. When it finishes
+            //that, it changes the innerHTML value of container
+            //to the value of astroString.
+            for(i=0; i<json.length; i++){
                 let skillString = "";
                 for(let j =0; j<json[i].skills.length;j++){
                     skillString+=json[i].skills[j];
@@ -15,22 +35,41 @@ addEventListener("load", function(){
                         skillString+=", ";
                     }
                 }
-                astroString+=
-                `
-                <div class="astronaut">
-                    <div class="bio">
-                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
-                        <ul>
-                            <li>Hours in space: ${json[i].hoursInSpace}</li>
-                            <li>Active: ${json[i].active}</li>
-                            <li>Skills: ${skillString}</li>
-                        </ul>
-                    </div>
-                    <img class="avatar" src=${json[i].picture}>
-                </div>`;
+                if(json[i].active === true){
+                    astroString+=
+                    `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li style="color:green">Active: ${json[i].active}</li>
+                                <li>Skills: ${skillString}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src=${json[i].picture}>
+                    </div>`;
+                } else if (json[i].active === false){
+                    astroString+=
+                    `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li style="color:red">Active: ${json[i].active}</li>
+                                <li>Skills: ${skillString}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src=${json[i].picture}>
+                    </div>`;
+                }
             }
+            astroString+=`
+            <div class="astronaut">
+            Total Astronauts: ${i}
+            </div>`
             container.innerHTML = astroString;
-            console.log(container.innerHTML);
 
         })
     });
